f04e0dd6aef5e574c912265f031e26f20f0c6299
spasojevicn <spasojevicnikolaaa@gmail.com>
2024-11-05 19:15:16 +0100

    Dodaj na develop

---
+++ isa/isa/src/main/java/com/onlybuns/isa/controller/AdminController.java
@@ -0,0 +1,35 @@
+package com.onlybuns.isa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminHome() {
+        return "adminHome";
+    }
+
+    // Dodajte mape za svaku od stranica
+    @GetMapping("/admin/posts")
+    public String viewPosts() {
+        return "adminPosts";
+    }
+
+    @GetMapping("/admin/trends")
+    public String viewTrends() {
+        return "adminTrends";
+    }
+
+    @GetMapping("/admin/analytics")
+    public String viewAnalytics() {
+        return "adminAnalytics";
+    }
+
+    @GetMapping("/admin/users")
+    public String viewUsers() {
+        return "adminUsers";
+    }
+}
+
