a44ab75ff4fc7dca4a4c7c3b6293c4a0edc7b7ac
Marija Mandic <mandicmarija27@gmail.com>
2025-03-12 21:17:36 +0100

    feat: implemented web socket

--- isa/isa/src/main/java/com/onlybuns/isa/IsaApplication.java
+++ isa/isa/src/main/java/com/onlybuns/isa/IsaApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@EnableRabbit
+//@EnableRabbit
 public class IsaApplication {
 
 	public static void main(String[] args) {
--- isa/isa/src/main/java/com/onlybuns/isa/controller/MessageController.java
+++ isa/isa/src/main/java/com/onlybuns/isa/controller/MessageController.java
@@ -1,23 +1,32 @@
 package com.onlybuns.isa.controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onlybuns.isa.service.RabbitMQProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.Map;
 @RestController
 @RequestMapping("/api/chat")
 public class MessageController {
 
-    @Autowired
-    private RabbitMQProducer rabbitMQProducer;
+    private final RabbitMQProducer rabbitMQProducer;
 
-    @PostMapping("/message")
-    public String sendMessage(@RequestParam String message) {
+    public MessageController(RabbitMQProducer rabbitMQProducer) {
+        this.rabbitMQProducer = rabbitMQProducer;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
         rabbitMQProducer.sendMessage(message);
-        return "Message sent: " + message;
+        return ResponseEntity.ok("Message sent to RabbitMQ: " + message);
     }
 }
-
